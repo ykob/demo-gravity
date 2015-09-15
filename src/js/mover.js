61c7ca62c33788ab362a8cdd99d3ea677df0eb0e
yoichi kobayashi <info@tplh.net>
2015-09-15 20:42:49 +0900

    [modify] 反発係数を調整。

--- src/js/mover.js
+++ src/js/mover.js
@@ -57,7 +57,7 @@ var exports = function(){
     rebound: function(vector) {
       var dot = this.acceleration.clone().dot(vector);
       this.acceleration.sub(vector.multScalar(2 * dot));
-      this.acceleration.multScalar(0.5);
+      this.acceleration.multScalar(0.666);
     },
     direct: function(vector) {
       var v = vector.clone().sub(this.position);
